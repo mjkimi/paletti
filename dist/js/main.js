dd954e2be6e749ad3f53d51e978f67e6ae3659c0
MG <meg@meg.com>
2019-04-03 12:41:04 +0300

    Image slider: markup, style and js

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,7 +23,6 @@ let active = false;
 function showForm() {
   const form = document.querySelector('.signin');
   const menuDialog = document.querySelector('.header-menu-dialog');
-  console.log('hi');
   if (!active) {
     form.className += ' ' + 'wer';
     // form.style.opacity = '1';
@@ -83,3 +82,67 @@ function closeMenu() {
   header.classList.remove('close');
 }
 dark.addEventListener('click', closeMenu);
+
+// Slider
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const controlBtn = document.querySelector('#control');
+let autoplay = true;
+let intervalTime = 3000;
+let slideInterval;
+
+const nextSlide = () => {
+  const current = document.querySelector('.current');
+  current.classList.remove('current');
+  // check for the next slide and pass on current class
+  if (current.nextElementSibling) {
+    current.nextElementSibling.classList.add('current');
+  } else {
+    // back to the 1st slide
+    slides[0].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'), 200);
+};
+
+const prevSlide = () => {
+  const current = document.querySelector('.current');
+  current.classList.remove('current');
+  // check for the prev slide and pass on current class
+  if (current.previousElementSibling) {
+    current.previousElementSibling.classList.add('current');
+  } else {
+    slides[slides.length - 1].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'), 200);
+};
+
+next.addEventListener('click', e => {
+  nextSlide();
+  resetTime();
+});
+prev.addEventListener('click', e => {
+  prevSlide();
+  resetTime();
+});
+
+function resetTime() {
+  clearInterval(slideInterval);
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
+
+// Slider autoplay control
+if (autoplay) {
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
+// Play/pause slide show
+controlBtn.addEventListener('click', e => {
+  if (autoplay) {
+    autoplay = false;
+    clearInterval(slideInterval);
+  } else {
+    autoplay = true;
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
